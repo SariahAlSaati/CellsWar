4fd92462e6584f768576a665aa72514abead0b1c
SariahAlSaati <sariah.al-saati@polytechnique.edu>
2020-04-26 16:57:06 +0200

    initial version of the game, with only a few features added. Will be the base of the project

--- CellsBattle/Assets/Scripts/Case.cs
+++ CellsBattle/Assets/Scripts/Case.cs
@@ -1,8 +1,52 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class Case : MonoBehaviour {
-    private int couleur;
+
+    // Contient IsNeighbour(Case deuxiemecase)
+    // Population Pi, population qu'il est possible d'envoyer
+    // Population Preceived: population reçue au cour du tour
+    // booléen HasReceived (inutile selon Ahmed)
+    // booléen HasSent (inutiles selon Ahmed)
+    // booléen  HasChangedTeam
+
+    
+
     private const int popmax = 20;
     public int pop;
     private int condition;
+
+    private Animator anim;
+    
+    enum CaseState {
+        neutral = 0,
+        plyr1 = 1,
+        plyr2 = 2,
+    }
+
+    CaseState casestate = CaseState.neutral;
+
+    void Start(){
+        pop = 1;
+        anim = GetComponentInChildren<Animator> ();
+        anim.SetInteger("AnimCaseState",(int)casestate);
+    }
+
+    void Update(){
+    }
+
+    //gère l'envoi des cellules avec varible globale de MapManager casedereference
+
+    void OnMouseOver(){
+        if(Input.GetMouseButtonDown(0)){
+            if (casestate == CaseState.plyr1) casestate = CaseState.plyr2 ;
+            else casestate = CaseState.plyr1;
+            
+            anim.SetInteger("AnimCaseState",(int)casestate);
+        }
+ }
+    
+
 }
---
+++ CellsBattle/Assets/Scripts/GUIHandler.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIHandler : MonoBehaviour
+{
+    public Transform button_PLAY;
+	public Transform button_RESIGN;
+    public Transform button_ENDTURN;
+    public Transform InputField;
+    public Transform TextPlayer;
+    public Transform TextMenu;
+
+    public const int _INGUI = 0;
+    public const int _PLAYER1 = 1;
+	public const int _PLAYER2 = 2;
+
+	
+
+	private static int _state = _INGUI;
+    
+    public static int state {
+				get {
+						return _state;
+				}
+				set {
+						_state = value;
+				}
+		}
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button playbtn = button_PLAY.GetComponent<Button> ();
+        playbtn.onClick.AddListener(TaskOnClickPlayButton);
+
+        Button resignbtn = button_RESIGN.GetComponent<Button> ();
+        resignbtn.onClick.AddListener(TaskOnClickResignButton);
+
+        Button endTurnbtn = button_ENDTURN.GetComponent<Button> ();
+        endTurnbtn.onClick.AddListener(TaskOnClickEndTurnButton);
+
+        button_RESIGN.gameObject.SetActive (false);
+
+        button_ENDTURN.gameObject.SetActive (false);
+        InputField.gameObject.SetActive (false);
+        TextPlayer.gameObject.SetActive (false);
+        TextMenu.gameObject.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+   }
+
+   void TaskOnClickPlayButton(){
+       button_PLAY.gameObject.SetActive (false);
+       button_RESIGN.gameObject.SetActive (true);
+       button_ENDTURN.gameObject.SetActive (true);
+       InputField.gameObject.SetActive (true);
+       TextPlayer.gameObject.SetActive (true);
+       TextMenu.gameObject.SetActive(false);
+       state = _PLAYER1;
+   }
+
+   void TaskOnClickResignButton(){
+       button_RESIGN.gameObject.SetActive (false);
+       button_ENDTURN.gameObject.SetActive (false);
+       button_PLAY.gameObject.SetActive (true);
+       InputField.gameObject.SetActive (false);
+       TextPlayer.gameObject.SetActive (false);
+       TextMenu.gameObject.SetActive(true);
+       state = _INGUI;       
+   }
+
+   void TaskOnClickEndTurnButton(){
+       if (state == _PLAYER1) state = _PLAYER2;
+       else state = _PLAYER1;
+   }
+}
